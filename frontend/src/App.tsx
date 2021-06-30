68c4f4c1f2abd6314e906fbfbce591cec212e81b
Thanakrit Mekon <thanakrit.mekon@gmail.com>
2021-06-30 22:20:22 +0700

    render prop and component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,37 @@
+import { count } from 'console';
 import React, { useEffect, useState }from 'react';
 import './App.css';
 
-// - - - Function component - - - //
+// - - - Class component demo - - - //
+type AppProps = {
+  message?: string; // add ? it mean ok if nothing has passed to message
+};
+type AppState = {
+  counter: number;
+};
+class App extends React.Component<AppProps, AppState> {
+  state: AppState = {
+    counter: 0,
+  };
 
+  render() {
+    // if nothing pass to message(prop) it will be "None" as below
+    return (
+      <div>
+        {this.props.message ? this.props.message : "None"}
+        <br />
+          Counter = {this.state.counter}
+        <br />
+        <button onClick={() => {this.setState({counter: this.state.counter+1});}}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// - - - Function component - - - //
+/*
 const App = () => {
   const [message, setMessage] = useState("test"); // use hook(useState)
   // useEffect instead componenntDidMount()
@@ -24,7 +53,7 @@ export default App;
 
 // - - - Class component - - - //
 
-/*
+
 type AppState = {
   message: string;
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// Pass "MESSAGE to App.tsx"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App message="MESSAGE"/> 
   </React.StrictMode>,
   document.getElementById('root')
 );
